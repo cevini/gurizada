83634d778ff05224b2459cd83823c7584c409b51
cevini <106355859+cevini@users.noreply.github.com>
2023-05-05 16:51:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-//Dropdown
-let click = document.querySelector('.click');
-let list = document.querySelector('.list');
-click.addEventListener("click",()=>{
-  list.classList.toggle('newlist')
-})
+
